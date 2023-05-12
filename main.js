643a8bfd6411e180c12dd16a43b91f87402ad72b
Edetmanchi <greatstory247@gmail.com>
2023-05-12 09:00:17 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const updateBtn = document.querySelector('.update-btn');
+const updateText = document.querySelector('.update-text')
+
+// function changeText(){
+//     update.innerHTML = "Hello"
+// }
+
+updateBtn.addEventListener('click', ()=>{
+    updateText.innerHTML = "I am currently learning Javascript"
+})
